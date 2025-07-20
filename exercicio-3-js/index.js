214a69f0fc9f48b7fb5517b6998ee4fc6e2f1923
Luanna <81788948+Luannabrandao@users.noreply.github.com>
2025-07-20 16:15:05 -0300

    adicionando novos arquivos

---
+++ exercicio-3-js/index.js
@@ -0,0 +1,9 @@
+let retornoAventureiro = confirm(
+  'Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?'
+);
+
+if (retornoAventureiro) {
+  alert('Ótimo! Nós temos as melhores camas de toda a região!');
+} else {
+  alert('Que pena! Você parecia ser uma pessoa mais legal');
+}
